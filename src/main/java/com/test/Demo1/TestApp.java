a19da67b0d660b9954d2c960b7dcbbd4937ba814
zhikang <>
2019-08-11 12:29:20 +0800

    test add

---
+++ src/main/java/com/test/Demo1/TestApp.java
@@ -0,0 +1,11 @@
+package com.test.Demo1;
+
+/**
+ * Created by hezhikang on 2019-08-11
+ */
+public class TestApp {
+
+    public static void main(String[] args) {
+        System.out.println("test FInish Hello Jin");
+    }
+}
